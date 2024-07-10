8728449d8fb3e4cb9c61171d061bb8459b65800e
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-10 15:14:19 +0300

    freeBookings

--- src/bookings/bookingsRouter.ts
+++ src/bookings/bookingsRouter.ts
@@ -7,12 +7,12 @@ import {
   updateBooking,
   deleteBooking,
 } from "./bookingController";
-import { adminRoleAuth, bothRoleAuth } from "../middleware/authBearer";
+import { bothRoleAuth } from "../middleware/authBearer";
 
 export const bookingRouter = new Hono();
 
-bookingRouter.get("/bookings",adminRoleAuth, getBookings);
-bookingRouter.get("/bookings/:id",adminRoleAuth, getBooking);
-bookingRouter.post("/bookings", adminRoleAuth,createBooking);
-bookingRouter.put("/bookings/:id", adminRoleAuth, updateBooking);
-bookingRouter.delete("/bookings/:id", bothRoleAuth, deleteBooking);
+bookingRouter.get("/bookings", getBookings);
+bookingRouter.get("/bookings/:id", getBooking);
+bookingRouter.post("/bookings", createBooking);
+bookingRouter.put("/bookings/:id", updateBooking);
+bookingRouter.delete("/bookings/:id", deleteBooking);
