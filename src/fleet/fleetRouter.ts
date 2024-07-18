51ae01d0f6d3288a89ccf2f41f660a17e0416570
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-18 10:14:32 +0300

    zod removal

--- src/fleet/fleetRouter.ts
+++ src/fleet/fleetRouter.ts
@@ -13,26 +13,16 @@ import { FleetManagementSchema } from "../validator";
 
 export const fleetRouter = new Hono();
 
-fleetRouter.get("/fleet",  getFleet);
+fleetRouter.get("/fleet", getFleet);
 fleetRouter.get("/fleet/:id", getVehicle);
 fleetRouter.post(
   "/fleet",
-  zValidator("json", FleetManagementSchema, (result, c) => {
-    if (!result.success) {
-      return c.json(result.error, 400);
-    }
-  }),
-  
+
   createVehicle
 );
 fleetRouter.put(
   "/fleet/:id",
-  zValidator("json", FleetManagementSchema, (result, c) => {
-    if (!result.success) {
-      return c.json(result.error, 400);
-    }
-  }),
-  
+
   updateVehicle
 );
 fleetRouter.delete("/fleet/:id", deleteVehicle);
