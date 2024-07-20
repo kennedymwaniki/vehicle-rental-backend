c478eba2098177f42e03458b2421e9b8a757cb53
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-07-20 17:27:38 +0300

    location

--- src/locations/locationService.ts
+++ src/locations/locationService.ts
@@ -1,5 +1,3 @@
-
-
 import { eq } from "drizzle-orm";
 import db from "../drizzle/db";
 import { TILocation, TSLocation, LocationsTable } from "../drizzle/schema";
@@ -9,7 +7,9 @@ export const getLocationsService = async () => {
   return locations;
 };
 
-export const getLocationById = async (id: number): Promise<TSLocation | undefined> => {
+export const getLocationById = async (
+  id: number
+): Promise<TSLocation | undefined> => {
   const location = await db.query.LocationsTable.findFirst({
     where: eq(LocationsTable.locationId, id),
   });
@@ -21,11 +21,18 @@ export const createLocationService = async (location: TILocation) => {
   return location;
 };
 
-export const updateLocationService = async (id: number, location: TILocation) => {
-  await db.update(LocationsTable).set(location).where(eq(LocationsTable.locationId, id));
+export const updateLocationService = async (
+  id: number,
+  location: TILocation
+) => {
+  await db
+    .update(LocationsTable)
+    .set(location)
+    .where(eq(LocationsTable.locationId, id));
   return location;
 };
 
 export const deleteLocationService = async (id: number) => {
   await db.delete(LocationsTable).where(eq(LocationsTable.locationId, id));
+  return "Location successfully deleted";
 };
